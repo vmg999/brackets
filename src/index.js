76ed38e363a74ffe7116595eadb170636eae7010
vmg999 <vmg999@gmail.com>
2020-10-03 00:30:06 +0300

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (
+                str[i] === bracketsConfig[j][0] &&
+                bracketsConfig[j][0] != bracketsConfig[j][1]
+            ) {
+                stack.push(str[i]);
+            } else if (
+                str[i] === bracketsConfig[j][1] &&
+                bracketsConfig[j][0] != bracketsConfig[j][1]
+            ) {
+                if (stack[stack.length - 1] === bracketsConfig[j][0]) {
+                    stack.pop();
+                } else {
+                    stack.push(str[i]);
+                }
+            } else if (
+                str[i] === bracketsConfig[j][0] &&
+                bracketsConfig[j][0] === bracketsConfig[j][1]
+            ) {
+                if (stack[stack.length - 1] === str[i]) {
+                    stack.pop();
+                } else if (stack[stack.length - 1] !== str[i]) {
+                    stack.push(str[i]);
+                }
+            }
+        }
+    }
+
+    if (stack.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
